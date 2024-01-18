8c7fa77a7be2fb99b38493e0611103600a7d67bf
Piotr Markiewicz <62255466+pio-mark@users.noreply.github.com>
2024-01-18 11:32:57 +0100

    Add files via upload

--- mark2.cpp
+++ mark2.cpp
@@ -1,188 +1,84 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <algorithm>
-#include <numeric>
 #include <limits>
+#include <cmath>
 #include <ctime>
-#include <cstdlib>
-#include <random>
 #include <thread>
 #include <mutex>
-#include <fstream>
 #include <json/json.h>
 
 using namespace std;
 
-mutex tabuListMutex;
 
-struct Solution {
-    vector<int> permutation;
+// Processing times for each job on each machine
+vector<vector<int>> processingTimes = loadProcessingTimesFromJSON("your_file_path.json");
 
-    Solution() = default;
+// Function to calculate makespan for a given permutation of jobs
+int calculateMakespan(const vector<int>& permutation) {
+    vector<int> completionTimes(numMachines, 0);
 
-    Solution(int n) {
-        for (int i = 0; i < n; ++i) {
-            permutation.push_back(i);
-        }
-        shuffle(permutation.begin(), permutation.end(), mt19937(random_device()()));
-    }
+    for (int jobIndex : permutation) {
+        completionTimes[0] += processingTimes[jobIndex][0];
 
-    Solution& operator=(const Solution& other) {
-        if (this != &other) {
-            permutation = other.permutation;
-        }
-        return *this;
-    }
-};
-
-int objectiveFunction(const Solution& solution, const vector<vector<int>>& processingTimes) {
-    int n = solution.permutation.size();
-    int m = processingTimes[0].size();
-    vector<int> completionTimes(m, 0);
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            completionTimes[j] += processingTimes[solution.permutation[i]][j];
-            if (i > 0 && j > 0) {
-                completionTimes[j] = max(completionTimes[j], completionTimes[j - 1]);
-            }
-            if (i == 0 && j == 0) {
-                completionTimes[j] = 0;
-            }
+        for (int machine = 1; machine < numMachines; ++machine) {
+            completionTimes[machine] = max(completionTimes[machine - 1], completionTimes[machine]) + processingTimes[jobIndex][machine];
         }
     }
 
-    return completionTimes.back();
+    return completionTimes[numMachines - 1];
 }
 
-Solution generateNeighbor(const Solution& currentSolution) {
-    Solution neighbor = currentSolution;
-    int n = neighbor.permutation.size();
-    int idx1 = rand() % n;
-    int idx2 = rand() % n;
-    swap(neighbor.permutation[idx1], neighbor.permutation[idx2]);
-    return neighbor;
+vector<int> generateRandomSolution() {
+    vector<int> solution(numJobs);
+    for (int i = 0; i < numJobs; ++i) {
+        solution[i] = i;
+    }
+    random_shuffle(solution.begin(), solution.end());
+    return solution;
 }
 
-int evaluateNeighbor(const Solution& neighbor, const vector<vector<int>>& processingTimes) {
-    return objectiveFunction(neighbor, processingTimes);
-}
+void tabuSearchWorker(int threadID, int maxIterations, int tabuListSize, vector<int>& localBestSolution, int& localBestMakespan) {
+    for (int iteration = 0; iteration < maxIterations; ++iteration) {
+        vector<int> currentSolution = generateRandomSolution();
 
-void generateAndEvaluateNeighbors(const Solution& currentSolution, const vector<vector<int>>& processingTimes,
-                                  int maxIterations, int tabuSize, int threadId, int numThreads,
-                                  Solution& localBestSolution, int& localBestObjective) {
-    int n = currentSolution.permutation.size();
-    vector<vector<int>> localTabuList(tabuSize, vector<int>(n, -1));
-
-    for (int iter = 0; iter < maxIterations; ++iter) {
-        Solution neighbor = generateNeighbor(currentSolution);
-        int neighborObjective = evaluateNeighbor(neighbor, processingTimes);
-        bool isTabu = false;
-
-        {
-            lock_guard<mutex> lock(tabuListMutex);
-            for (const auto& tabuMove : localTabuList) {
-                if (tabuMove == neighbor.permutation) {
-                    isTabu = true;
-                    break;
-                }
+        vector<vector<int>> neighbors;
+        for (int i = 0; i < numJobs - 1; ++i) {
+            for (int j = i + 1; j < numJobs; ++j) {
+                vector<int> neighbor = currentSolution;
+                swap(neighbor[i], neighbor[j]);
+                neighbors.push_back(neighbor);
             }
         }
 
-        if (!isTabu && neighborObjective < localBestObjective) {
-            localBestSolution = neighbor;
-            localBestObjective = neighborObjective;
+        int minNeighborMakespan = numeric_limits<int>::max();
+        vector<int> bestNeighbor;
+
+        for (const vector<int>& neighbor : neighbors) {
+            int neighborMakespan = calculateMakespan(neighbor);
+
+            if (neighborMakespan < minNeighborMakespan) {
+                minNeighborMakespan = neighborMakespan;
+                bestNeighbor = neighbor;
+            }
         }
 
-        {
-            lock_guard<mutex> lock(tabuListMutex);
-            localTabuList.push_back(neighbor.permutation);
-            if (localTabuList.size() > tabuSize) {
-                localTabuList.erase(localTabuList.begin());
-            }
+        currentSolution = bestNeighbor;
+        int currentMakespan = minNeighborMakespan;
+
+        if (currentMakespan < localBestMakespan) {
+            localBestSolution = currentSolution;
+            localBestMakespan = currentMakespan;
         }
     }
 }
 
-Solution tabuSearch(const vector<vector<int>>& processingTimes, int maxIterations, int tabuSize) {
-    int n = processingTimes.size();
-    Solution currentSolution(n);
-    Solution bestSolution = currentSolution;
-    int currentObjective = objectiveFunction(currentSolution, processingTimes);
-    int bestObjective = currentObjective;
-
-    vector<vector<int>> tabuList(tabuSize, vector<int>(n, -1));
-
-    for (int iter = 0; iter < maxIterations; ++iter) {
-        Solution neighbor = generateNeighbor(currentSolution);
-        int neighborObjective = evaluateNeighbor(neighbor, processingTimes);
-
-        bool isTabu = false;
-        for (const auto& tabuMove : tabuList) {
-            if (tabuMove == neighbor.permutation) {
-                isTabu = true;
-                break;
-            }
-        }
-
-        if (!isTabu && neighborObjective < currentObjective) {
-            currentSolution = neighbor;
-            currentObjective = neighborObjective;
-
-            if (currentObjective < bestObjective) {
-                bestSolution = currentSolution;
-                bestObjective = currentObjective;
-            }
-        }
-
-        tabuList.push_back(currentSolution.permutation);
-        if (tabuList.size() > tabuSize) {
-            tabuList.erase(tabuList.begin());
-        }
-    }
-
-    return bestSolution;
-}
-
-void parallelTabuSearch(const vector<vector<int>>& processingTimes, int maxIterations, int tabuSize, int numThreads) {
-    vector<thread> threads;
-    vector<Solution> bestSolutions(numThreads);
-    vector<int> bestObjectives(numThreads, numeric_limits<int>::max());
-
-    for (int t = 0; t < numThreads; ++t) {
-        threads.emplace_back([&, t]() {
-            Solution currentSolution(processingTimes.size());
-            Solution localBestSolution = currentSolution;
-            int currentObjective = objectiveFunction(currentSolution, processingTimes);
-            int localBestObjective = currentObjective;
-
-            generateAndEvaluateNeighbors(currentSolution, processingTimes, maxIterations, tabuSize, t, numThreads,
-                                         localBestSolution, localBestObjective);
-
-            bestSolutions[t] = localBestSolution;
-            bestObjectives[t] = localBestObjective;
-        });
-    }
-
-    for (auto& thread : threads) {
-        thread.join();
-    }
-
-    int globalBestIndex = min_element(bestObjectives.begin(), bestObjectives.end()) - bestObjectives.begin();
-    Solution globalBestSolution = bestSolutions[globalBestIndex];
-
-    cout << "Najlepsza permutacja: ";
-    for (int task : globalBestSolution.permutation) {
-        cout << task << " ";
-    }
-    cout << "\nCzas przetwarzania wszystkich zadań na wszystkich maszynach: "
-         << objectiveFunction(globalBestSolution, processingTimes) << endl;
-}
-
+// Function to load processing times from a JSON file
 vector<vector<int>> loadProcessingTimesFromJSON(const string& filePath) {
     vector<vector<int>> processingTimes;
-    ifstream file(filePath);
 
+    ifstream file(filePath);
     if (file.is_open()) {
         Json::Value root;
         file >> root;
@@ -204,25 +100,41 @@ vector<vector<int>> loadProcessingTimesFromJSON(const string& filePath) {
 }
 
 int main() {
-    srand(static_cast<unsigned>(time(0)));
+    srand(time(0));
 
-    string filePath = "data/100_10.json";
-    vector<vector<int>> processingTimes = loadProcessingTimesFromJSON(filePath);
-
-    int maxIterations = 100;
-    int tabuSize = 10;
     int numThreads = 4;
+    int maxIterationsPerThread = 1000;
+    int tabuListSize = 25;
 
-    Solution result = tabuSearch(processingTimes, maxIterations, tabuSize);
+    vector<thread> threads;
+    vector<vector<int>> localBestSolutions(numThreads);
+    vector<int> localBestMakespans(numThreads, numeric_limits<int>::max());
 
-    cout << "Najlepsza permutacja: ";
-    for (int task : result.permutation) {
-        cout << task << " ";
+    for (int i = 0; i < numThreads; ++i) {
+        threads.emplace_back(tabuSearchWorker, i, maxIterationsPerThread, tabuListSize, ref(localBestSolutions[i]), ref(localBestMakespans[i]));
     }
-    cout << "\nCzas przetwarzania wszystkich zadań na wszystkich maszynach: "
-         << objectiveFunction(result, processingTimes) << endl;
 
-    parallelTabuSearch(processingTimes, maxIterations, tabuSize, numThreads);
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    int globalBestMakespan = numeric_limits<int>::max();
+    vector<int> globalBestSolution;
+
+    for (int i = 0; i < numThreads; ++i) {
+        if (localBestMakespans[i] < globalBestMakespan) {
+            globalBestMakespan = localBestMakespans[i];
+            globalBestSolution = localBestSolutions[i];
+        }
+    }
+
+    cout << "Global Best Solution - Optimal Permutation: ";
+    for (int jobIndex : globalBestSolution) {
+        cout << jobIndex << " ";
+    }
+    cout << endl;
+
+    cout << "Global Best Solution - Optimal Makespan: " << globalBestMakespan << endl;
 
     return 0;
 }
