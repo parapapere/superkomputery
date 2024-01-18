1f8bf23da4a12dfef942e388c4f3820bec80f4a9
Piotr Markiewicz <62255466+pio-mark@users.noreply.github.com>
2024-01-18 11:47:19 +0100

    Add files via upload

--- mark2.cpp
+++ mark2.cpp
@@ -6,17 +6,15 @@
 #include <cmath>
 #include <ctime>
 #include <thread>
-#include <mutex>
 #include <json/json.h>
 
 using namespace std;
 
-
-// Processing times for each job on each machine
-vector<vector<int>> processingTimes = loadProcessingTimesFromJSON("your_file_path.json");
-
 // Function to calculate makespan for a given permutation of jobs
-int calculateMakespan(const vector<int>& permutation) {
+int calculateMakespan(const vector<int>& permutation, const vector<vector<int>>& processingTimes) {
+    int numJobs = permutation.size();
+    int numMachines = processingTimes[0].size();
+
     vector<int> completionTimes(numMachines, 0);
 
     for (int jobIndex : permutation) {
@@ -30,7 +28,7 @@ int calculateMakespan(const vector<int>& permutation) {
     return completionTimes[numMachines - 1];
 }
 
-vector<int> generateRandomSolution() {
+vector<int> generateRandomSolution(int numJobs) {
     vector<int> solution(numJobs);
     for (int i = 0; i < numJobs; ++i) {
         solution[i] = i;
@@ -39,9 +37,11 @@ vector<int> generateRandomSolution() {
     return solution;
 }
 
-void tabuSearchWorker(int threadID, int maxIterations, int tabuListSize, vector<int>& localBestSolution, int& localBestMakespan) {
+void tabuSearchWorker(int threadID, int maxIterations, int tabuListSize, vector<int>& localBestSolution, int& localBestMakespan, const vector<vector<int>>& processingTimes) {
+    int numJobs = processingTimes.size();
+    
     for (int iteration = 0; iteration < maxIterations; ++iteration) {
-        vector<int> currentSolution = generateRandomSolution();
+        vector<int> currentSolution = generateRandomSolution(numJobs);
 
         vector<vector<int>> neighbors;
         for (int i = 0; i < numJobs - 1; ++i) {
@@ -56,7 +56,7 @@ void tabuSearchWorker(int threadID, int maxIterations, int tabuListSize, vector<
         vector<int> bestNeighbor;
 
         for (const vector<int>& neighbor : neighbors) {
-            int neighborMakespan = calculateMakespan(neighbor);
+            int neighborMakespan = calculateMakespan(neighbor, processingTimes);
 
             if (neighborMakespan < minNeighborMakespan) {
                 minNeighborMakespan = neighborMakespan;
@@ -102,16 +102,21 @@ vector<vector<int>> loadProcessingTimesFromJSON(const string& filePath) {
 int main() {
     srand(time(0));
 
+    string filePath = "your_file_path.json";
+    vector<vector<int>> processingTimes = loadProcessingTimesFromJSON(filePath);
+
+    int numJobs = processingTimes.size();
+    
     int numThreads = 4;
-    int maxIterationsPerThread = 1000;
-    int tabuListSize = 25;
+    int maxIterationsPerThread = 25;
+    int tabuListSize = 5;
 
     vector<thread> threads;
     vector<vector<int>> localBestSolutions(numThreads);
     vector<int> localBestMakespans(numThreads, numeric_limits<int>::max());
 
     for (int i = 0; i < numThreads; ++i) {
-        threads.emplace_back(tabuSearchWorker, i, maxIterationsPerThread, tabuListSize, ref(localBestSolutions[i]), ref(localBestMakespans[i]));
+        threads.emplace_back(tabuSearchWorker, i, maxIterationsPerThread, tabuListSize, ref(localBestSolutions[i]), ref(localBestMakespans[i]), processingTimes);
     }
 
     for (auto& t : threads) {
